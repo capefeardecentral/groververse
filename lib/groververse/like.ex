da28d54ff1c39a817c9edadf29f4aa71fe329913
Cape Fear Decentral <info@capefeardecentral.net>
2023-01-18 16:53:12 -0500

    add likes schema

---
+++ lib/groververse/like.ex
@@ -0,0 +1,25 @@
+defmodule Groververse.Like do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "likes" do
+
+    field :user_id, :id
+    field :post_id, :id
+
+    timestamps()
+  end
+
+  def like_post(attrs) do
+    %Like{}
+    |> Like.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc false
+  def changeset(like, attrs) do
+    like
+    |> cast(attrs, [:user_id, :post_id])
+    |> validate_required([:user_id, :post_id])
+  end
+end
